68ca950eb64fed323b6f44b6a95530999724be3d
Mumtahin Farabi <mfarabi619@gmail.com>
2024-06-29 14:49:52 -0400

    chore(Root): Add Shadcn/ui

--- nextjs-starter-docs/app/page.tsx
+++ nextjs-starter-docs/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { Button } from '@/components/ui/button'
 
 export default function HomePage() {
   return (
@@ -6,9 +7,11 @@ export default function HomePage() {
       <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
       <p className="text-muted-foreground">
         You can open{' '}
-        <Link href="/docs" className="text-foreground font-semibold underline">
-          /docs
-        </Link>{' '}
+        <Button variant="secondary" asChild>
+          <Link href="/docs" className="text-foreground font-semibold underline">
+            /docs
+          </Link>
+        </Button>{' '}
         and see the documentation.
       </p>
     </main>
--- nextjs-starter-docs/tailwind.fumadocs.config.ts
+++ nextjs-starter-docs/tailwind.fumadocs.config.ts
@@ -2,6 +2,8 @@ import { createPreset } from 'fumadocs-ui/tailwind-plugin';
 import type { Config } from "tailwindcss"
 
 const config= {
+  darkMode: ["class"],
+  prefix: "",
   content: [
     './app/docs/**/*.{ts,tsx}',
     './content/**/*.{md,mdx}',
