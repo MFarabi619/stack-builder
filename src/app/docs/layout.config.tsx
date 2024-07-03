0938806f0174b9e8c2796590baf491ccf7de4148
Mumtahin Farabi <mfarabi619@gmail.com>
2024-07-02 21:33:23 -0400

    feat(Docs): Add icons, Root toggle, pages, and re-structure files

--- src/app/docs/layout.config.tsx
+++ src/app/docs/layout.config.tsx
@@ -1,16 +1,24 @@
 import type { BaseLayoutProps, DocsLayoutProps } from 'fumadocs-ui/layout'
-import { pageTree } from '@/app/docs/source'
+import { RootToggle } from 'fumadocs-ui/components/layout/root-toggle'
+import { Library as DocumentationPageIcon, Layers as StackBuilderIcon } from 'lucide-react'
+import { pageTree, pages } from '@/app/docs/source'
 
 // shared configuration
 export const baseOptions: BaseLayoutProps = {
   nav: {
-    title: 'Stack Builder Docs',
+    title: (
+      <>
+        <StackBuilderIcon />
+        <span className="text-lg font-bold">Stack Builder</span>
+      </>
+    ),
   },
   githubUrl: 'https://github.com/MFarabi619/stack-builder',
   links: [
     {
-      text: 'Documentation',
+      text: 'Home',
       url: '/docs',
+      icon: <DocumentationPageIcon />,
       active: 'nested-url',
     },
   ],
@@ -24,5 +32,25 @@ export const homeOptions: BaseLayoutProps = {
 // docs layout configuration
 export const docsOptions: DocsLayoutProps = {
   ...baseOptions,
+  sidebar: {
+    banner: (
+      <RootToggle
+        options={pages.map(page => ({
+          title: page.title,
+          description: page.description,
+          url: `/docs/${page.url}`,
+          icon: (
+            <page.icon
+              className="size-9 shrink-0 rounded-md bg-gradient-to-t from-background/80 p-1.5"
+              style={{
+                backgroundColor: `hsl(var(--primary)/.3)`,
+                color: `hsl(var(--accent-foreground))`,
+              }}
+            />
+          ),
+        }))}
+      />
+    ),
+  },
   tree: pageTree,
 }
--- src/app/docs/source.ts
+++ src/app/docs/source.ts
@@ -4,8 +4,39 @@ import { loader } from 'fumadocs-core/source'
 import { createElement } from 'react'
 
 import {
+  Palette as DesignIcon,
+  GitPullRequestCreateArrow as DevelopmentIcon,
+  Dock as HomeIcon,
+  Kanban as ProjectManagementIcon,
   icons,
 } from 'lucide-react'
+
+export const pages = [
+  {
+    title: 'Home',
+    description: 'Best decisions start with the best information',
+    url: '',
+    icon: HomeIcon,
+  },
+  {
+    title: 'Project Management',
+    description: 'Turn visions into reality with confidence',
+    url: 'project-management',
+    icon: ProjectManagementIcon,
+  },
+  {
+    title: 'Design',
+    description: 'Deliver outstanding UI/UX at high velocity',
+    url: 'design',
+    icon: DesignIcon,
+  },
+  {
+    title: 'Development',
+    description: 'Architect your solution for scale and longevity',
+    url: 'development',
+    icon: DevelopmentIcon,
+  },
+]
 
 export const { getPage, getPages, pageTree } = loader({
   baseUrl: '/docs',
