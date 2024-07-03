446d2520537331412e1f4811a56e0b7db2b98a9f
Mumtahin Farabi <mfarabi619@gmail.com>
2024-07-02 21:04:40 -0400

    chore: Install Lucide React icons

--- src/app/docs/source.ts
+++ src/app/docs/source.ts
@@ -1,9 +1,25 @@
 import { map } from '@root/.map'
 import { createMDXSource } from 'fumadocs-mdx'
 import { loader } from 'fumadocs-core/source'
+import { createElement } from 'react'
+
+import {
+  icons,
+} from 'lucide-react'
 
 export const { getPage, getPages, pageTree } = loader({
   baseUrl: '/docs',
   rootDir: 'docs',
+  icon(icon) {
+    if (!icon) {
+      // You may set a default icon
+      // return createElement(HomeIcon)
+      return
+    }
+
+    if (icon in icons)
+      return createElement(icons[icon as keyof typeof icons])
+  },
+
   source: createMDXSource(map),
 })
