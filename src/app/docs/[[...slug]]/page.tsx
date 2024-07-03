17039d40fdb79df8a34b46b04d23cf2718d5db02
Mumtahin Farabi <mfarabi619@gmail.com>
2024-07-02 23:44:30 -0400

    feat(Docs): Add 'Edit on GitHub' button

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { DocsBody, DocsPage } from 'fumadocs-ui/page'
 import { notFound } from 'next/navigation'
 import { RollButton } from 'fumadocs-ui/components/roll-button'
+import { Edit } from 'lucide-react'
+import packageJson from '@root/package.json'
 import { getPage, getPages } from '@/app/docs/source'
 
 export default async function Page({
@@ -16,12 +18,30 @@ export default async function Page({
   }
 
   const MDX = page.data.exports.default
+  const path = `src/content/docs/${page.file.path}`
+  const gitHubRepoUrl = packageJson.repository.url.replace(/\.git$/, '') // Remove .git suffix
+
+  const footer = (
+    <a
+      href={`${gitHubRepoUrl}/blob/main/${path}`}
+      target="_blank"
+      rel="noreferrer noopener"
+      className="inline-flex items-center justify-center rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border bg-secondary text-secondary-foreground hover:bg-secondary/80 h-9 px-3 text-xs gap-1.5"
+    >
+      <Edit className="size-3" />
+      Edit on GitHub
+    </a>
+  )
 
   return (
     <DocsPage
       toc={page.data.exports.toc}
       lastUpdate={page.data.exports.lastModified}
       full={page.data.full}
+      tableOfContent={{
+        footer,
+      }}
+      tableOfContentPopover={{ footer }}
     >
       <RollButton />
       <DocsBody>
