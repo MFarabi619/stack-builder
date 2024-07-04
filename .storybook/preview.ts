493584b4804d4dada1756f43a73ecae1a0396086
Mumtahin Farabi <mfarabi619@gmail.com>
2024-07-04 19:13:40 -0400

    chore(Storybook): Configure theme to dark default

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react'
+import { themes } from '@storybook/theming'
 
 const preview: Preview = {
   parameters: {
@@ -8,6 +9,7 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    docs: { theme: themes.dark },
   },
 }
 
