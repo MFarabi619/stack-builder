4d69a0d848862d4b47f7ce7f4e9fbbb398878b12
Mumtahin Farabi <mfarabi619@gmail.com>
2024-07-05 00:12:28 -0400

    chore(Storybook): Build Orbitkit Storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,14 +5,23 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false,
+      },
+    },
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    'storybook-dark-mode',
   ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
   staticDirs: ['../public'],
+  docs: {
+    autodocs: 'tag',
+  },
 }
 export default config
--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,6 +1,36 @@
-import { addons } from '@storybook/manager-api'
-import { themes } from '@storybook/theming'
+import { useEffect } from 'react'
+
+import { addons, types, useGlobals } from '@storybook/manager-api'
+
+import theme from './theme'
 
 addons.setConfig({
-  // theme: themes.dark,
+  theme,
 })
+
+function ExampleToolbar() {
+  const [globals] = useGlobals()
+
+  useEffect(() => {
+    const elements = document.querySelectorAll('.docs-story')
+
+    elements.forEach((element) => {
+      element.classList.add(globals.theme as string)
+    })
+  }, [globals])
+
+  return null
+}
+
+export function registerAddons() {
+  addons.register('docs-theme', () => {
+    addons.add('docs-theme-addon', {
+      title: 'Addon to change docs story theme',
+      type: types.TOOL,
+      match: ({ viewMode }) => !!viewMode?.match(/^(?:story|docs)$/),
+      render: ExampleToolbar,
+    })
+  })
+}
+
+registerAddons()
