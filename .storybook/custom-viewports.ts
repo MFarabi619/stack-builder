ddb6f7485e42c17af6cfb4bc41d79dc5b829947b
Mumtahin Farabi <mfarabi619@gmail.com>
2024-07-11 22:18:50 -0400

    refactor(storybook): improve configuration files readability

---
+++ .storybook/custom-viewports.ts
@@ -0,0 +1,37 @@
+export const customViewports = {
+  '720p': {
+    name: '720p',
+    styles: {
+      width: '1280px',
+      height: '720px',
+    },
+  },
+  '1080p': {
+    name: '1080p',
+    styles: {
+      width: '1920px',
+      height: '1080px',
+    },
+  },
+  '2k': {
+    name: '2K',
+    styles: {
+      width: '2560px',
+      height: '1440px',
+    },
+  },
+  '4k': {
+    name: '4K',
+    styles: {
+      width: '3840px',
+      height: '2160px',
+    },
+  },
+  '21/9': {
+    name: '21/9',
+    styles: {
+      width: '2560px',
+      height: '1080px',
+    },
+  },
+}
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,60 +1,44 @@
+import React from 'react'
 import type { Preview } from '@storybook/react'
 
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
-import { ThemeProvider } from 'next-themes'
-import type { ThemeVars } from '@storybook/theming'
-import { themes } from '@storybook/theming'
-import { withThemeByClassName } from '@storybook/addon-themes'// Wide button with a pen and text. Toggles both Preview Components and Preview Background
 
+import { ThemeProvider } from 'next-themes'
+import { withThemeByClassName } from '@storybook/addon-themes'// Wide button with a pen and text. Toggles both Preview Components and Preview Background
+import { type ThemeVars, themes } from '@storybook/theming'
 import { DARK_MODE_EVENT_NAME } from 'storybook-dark-mode'
-import React from 'react'
+
 import { DocsContainer, type DocsContextProps } from '@storybook/blocks'
 
+import { customViewports } from './custom-viewports'
 import { commonTheme, darkUIStorybook, lightUIStorybook } from './themes-storybook-ui'
-
 import './globals.css'
-
-const customViewports = {
-  '720p': {
-    name: '720p',
-    styles: {
-      width: '1280px',
-      height: '720px',
-    },
-  },
-  '1080p': {
-    name: '1080p',
-    styles: {
-      width: '1920px',
-      height: '1080px',
-    },
-  },
-  '2k': {
-    name: '2K',
-    styles: {
-      width: '2560px',
-      height: '1440px',
-    },
-  },
-  '4k': {
-    name: '4K',
-    styles: {
-      width: '3840px',
-      height: '2160px',
-    },
-  },
-  '21/9': {
-    name: '21/9',
-    styles: {
-      width: '2560px',
-      height: '1080px',
-    },
-  },
-}
 
 const preview: Preview = {
   parameters: {
     layout: 'centered',
+    actions: {
+      argTypesRegex: '^on[A-Z].*',
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    options: {
+      storySort: {
+        method: 'alphabetical',
+        order: ['Configure your project', 'Zenith Design System', 'Shadcn-ui', 'Storybook Starters'],
+      },
+    },
+    // https://storybook.js.org/docs/essentials/viewport
+    viewport: {
+      viewports: {
+        ...INITIAL_VIEWPORTS,
+        ...customViewports,
+      },
+    },
     // globalTypes: {
     //   theme: {
     //     description: 'Global theme for components',
@@ -91,25 +75,27 @@ const preview: Preview = {
         ...commonTheme,
       },
     },
-    actions: {
-      argTypesRegex: '^on[A-Z].*',
-    },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-    options: {
-      storySort: {
-        method: 'alphabetical',
-        order: ['Configure your project', 'Introduction', 'Examples', 'Shadcn', 'Colors', 'Typography'],
-      },
-    },
-    viewport: {
-      viewports: {
-        ...INITIAL_VIEWPORTS,
-        ...customViewports,
+    // https://storybook.js.org/docs/essentials/backgrounds
+    backgrounds: {
+      // disable: true,
+      default: 'twitter',
+      values: [
+        {
+          name: 'twitter',
+          value: '#00aced',
+        },
+        {
+          name: 'facebook',
+          value: '#3b5998',
+        },
+      ],
+      grid: {
+        // disable: true,
+        // cellSize: 20,
+        // opacity: 0.5,
+        // cellAmount: 5,
+        // offsetX: 16, // Default is 0 if story has 'fullscreen' layout, 16 if layout is 'padded'
+        // offsetY: 16, // Default is 0 if story has 'fullscreen' layout, 16 if layout is 'padded'
       },
     },
     // fix for theming docs page found here: https://github.com/hipstersmoothie/storybook-dark-mode/issues/282#issuecomment-2208816632
