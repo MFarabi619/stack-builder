82ec5ebda6f07891cf1a792b348727ade0757e9d
Mumtahin Farabi <mfarabi619@gmail.com>
2024-07-13 19:27:30 -0400

    refactor(home): remove nonexistent type from home button

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,15 @@
-import { HomePageButton, type HomePageButtonType } from '@/components/ui/home-button'
+import { HomePageButton } from '@/components/ui/home-button'
 import { buttons } from '@/content/web/buttons'
 
 export default function HomePage() {
   return (
     <main className="flex h-screen flex-col justify-center items-center text-center max-w-lg mx-auto">
       {buttons.slice(0, 1).map(button => (
-        <HomePageButton key={button.href} {...(button as HomePageButtonType)} />
+        <HomePageButton key={button.href} {...(button)} />
       ))}
-      <section className="flex space-x-2">
+      <section className="flex flex-col space-y-3 sm:flex-row sm:space-x-2 sm:space-y-0">
         {buttons.slice(1).map(button => (
-          <HomePageButton key={button.href} {...(button as HomePageButtonType)} />
+          <HomePageButton key={button.href} {...(button)} />
         ))}
       </section>
     </main>
